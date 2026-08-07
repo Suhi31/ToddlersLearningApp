ff7f9cd2533f4a1a981b998907c4c0818953805e
suhi31 <nusrat201614031@gmail.com>
2026-08-07 16:21:23 +0600

    Initial Commit

---
+++ ToddlerLearningApp/ToddlerLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToddlerLearningAppApp.swift
+//  ToddlerLearningApp
+//
+//  Created by Nusrat Jahan on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ToddlerLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
